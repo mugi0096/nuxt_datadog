4af89b6aa5338b0e565cfb9d37068886752c6495
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-09-21 22:43:01 +0900

    💄 add text style to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+ const msg = 'Hello World!'
 </script>
 
 <template>
   <div>
-    <h1>Home</h1>
+    <h1 class="text-lg font-bold">Home</h1>
+    <p class="mt-2 text-sm">{{ msg }}</p>
   </div>
 </template>
