6bdcc679014a2cfff435cc7827058055897d6706
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-09-21 23:00:09 +0900

    ✨ add header

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="flex items-center pb-2 px-2">
+    <NuxtLink to="/" class="me-auto text-lg font-bold text-violet-600">Datadog Demo</NuxtLink>
+    <NuxtLink to="/" class="p-1 hover:bg-violet-300">Home</NuxtLink>
+    <NuxtLink to="/about" class="p-1 hover:bg-violet-300">About</NuxtLink>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Header />
+    <div class="flex justify-center items center">
+      <slot />
+    </div>
+  </div>
+</template>
