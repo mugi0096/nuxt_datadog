b9a48a902c4ea8622da18247b815f00ad20092cf
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-09-29 16:42:14 +0900

    🎨 add function to buttons

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,18 @@
 <script setup>
-const handleClickNormalBUtton = () => {
-  alert('Hello World!')
-}
+import { datadogRum } from "@datadog/browser-rum";
 
-const handleClickErrorBUtton = () => {
-  throw new Error('エラーが発生しました')
-}
+const handleClickNormalButton = () => {
+  alert("Hello World!");
+};
+
+const handleClickErrorButton = () => {
+  throw new Error("エラーが発生しました");
+};
+
+const handleClickErrorStockButton = () => {
+  const error = new Error("収集されるエラー");
+  datadogRum.addError(error, { message: "エラーストックに収集されました" });
+};
 </script>
 
 <template>
@@ -13,21 +20,29 @@ const handleClickErrorBUtton = () => {
     <h1 class="text-lg font-bold">About</h1>
     <p class="mt-2 text-sm">このページでインタラクションを実装</p>
     <div class="flex flex-col">
-      <button 
-      class="mt-2 bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded"
-      @click="handleClickNormalBUtton">
-      普通のボタン
-    </button>
-    <button
-      class="mt-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-      @click="handleClickErrorBUtton">
-      エラーボタン
-    </button>
-    <button
-      class="mt-2 bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded"
-    >
-      何もできないボタン
-    </button>
+      <button
+        class="mt-2 bg-violet-500 hover:bg-violet-600 text-white font-bold py-2 px-4 rounded"
+        @click="handleClickNormalButton"
+      >
+        普通のボタン
+      </button>
+      <button
+        class="mt-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+        @click="handleClickErrorButton"
+      >
+        エラーボタン
+      </button>
+      <button
+        class="mt-2 bg-red-800 hover:bg-red-900 text-white font-bold py-2 px-4 rounded"
+        @click="handleClickErrorStockButton"
+      >
+        エラーボタン(without console error)
+      </button>
+      <button
+        class="mt-2 bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded"
+      >
+        何もできないボタン
+      </button>
     </div>
   </div>
 </template>
